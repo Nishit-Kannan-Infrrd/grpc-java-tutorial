aa327cfb88f4ec975fb7ca225f0d2534187da7d4
Nishit Kannan <niskan@gmail.com>
2021-12-01 14:22:10 +0530

    Docker file for grpc server

--- grpc/src/main/java/com/crazynerve/grpctutorials/App.java
+++ grpc/src/main/java/com/crazynerve/grpctutorials/App.java
@@ -5,6 +5,7 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 
 /**
@@ -13,12 +14,16 @@ import java.io.IOException;
  */
 public class App 
 {
+    private static final Logger LOGGER = Logger.getLogger( App.class.getName() );
+
     public static void main( String[] args ) throws IOException, InterruptedException
     {
         Server server = ServerBuilder.forPort( 5050 )
             .addService( new NumberServiceImpl() )
             .build();
+        LOGGER.info( () -> "Starting server..." );
         server.start();
+        LOGGER.info( () -> "Server started..." );
         server.awaitTermination();
     }
 }
